d1a79cde15237f26b7ed5b79a622021b566b8bd1
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-08 15:52:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -190,9 +190,7 @@ if selecteds == 1:
         """
         st.code(code, language="python")
         st.text("Output")
-        st.success("45",/n
-            "1456.8",/n
-            "John")
+        st.success("45\n1456.8\nJohn")
 
 
 
