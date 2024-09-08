95f95737e520b7b62ce53bed26bb393c1f1145cf
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-08 14:29:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,5 +146,5 @@ if selecteds == 1:
         st.subheader("Indentation in Python")
         st.write("Python indentation refers to adding white space before a statement to a particular block of code. In other words, all the statements with the same space to the left, belong to the same code block.")
         image_url1 = "https://media.geeksforgeeks.org/wp-content/uploads/20191125112615/Indentation-python2.jpg"
-        st.image(image_url1, width=800)
+        st.image(image_url1, width=600)
     
