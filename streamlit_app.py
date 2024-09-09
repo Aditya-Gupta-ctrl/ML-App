0a47b1dc4dee97e1d6cf74dbb7d0657452e252a5
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-09 17:45:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -452,7 +452,30 @@ Multi-Dimensional List:
 """
         st.code(code7, language="python")
         st.subheader("Python Access List Items")
-        st.write("")
+        st.write("In order to access the list items refer to the index number. Use the index operator [ ] to access an item in a list. In Python, negative sequence indexes represent positions from the end of the array. Instead of having to compute the offset as in List[len(List)-3], it is enough to just write List[-3]. Negative indexing means beginning from the end, -1 refers to the last item, -2 refers to the second-last item, etc.")
+        st.text("Input")
+        code8 = """
+List = ["Geeks", "For", "Geeks"]
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+print("Accessing element using negative indexing")
+print(List[-1])
+print(List[-3])
+"""
+        st.code(code8, language="python")
+        st.text("Output")
+        code9 = """
+Accessing element from the list
+Geeks
+Geeks
+Accessing element using negative indexing
+Geeks
+Geeks
+"""
+        st.code(code9, language="python")
+        
+        
         
         
         
