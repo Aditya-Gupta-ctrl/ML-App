aec3fc05e3885a6eb5580e489c53e78c32f01f6a
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-10 12:43:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -543,8 +543,18 @@ Third last element of tuple
 """
         st.code(code13, language="python")
         
-        
-        
+        st.subheader("3. Boolean Data Type in Python")
+        st.write("Python Data type with one of the two built-in values, True or False. Boolean objects that are equal to True are truthy (true), and those equal to False are falsy (false). However non-Boolean objects can be evaluated in a Boolean context as well and determined to be true or false. It is denoted by the class bool.")
+        st.info("Note – True and False with capital ‘T’ and ‘F’ are valid booleans otherwise python will throw an error.")
+        st.markdown("""
+        **Example:** The first two lines will print the type of the boolean values True and False, which is <class ‘bool’>. The third line will cause an error, because true is not a valid keyword in Python. Python is case-sensitive, which means it distinguishes between uppercase and lowercase letters. You need to capitalize the first letter of true to make it a boolean value.
+        """)
+        code14 = """
+print(type(True))
+print(type(False))
+print(type(true))
+"""
+        st.code(code14, language="python")
 
         
 
