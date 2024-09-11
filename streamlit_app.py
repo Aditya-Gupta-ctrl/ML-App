08b9e91d73b61e53c78211b5c44a54ab90392ef5
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-11 10:46:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ selecteds = sac.tabs([
     sac.TabsItem(label='C', icon=''),
     sac.TabsItem(label='C++', icon=''),
     sac.TabsItem(label='Java', icon=''),
-    sac.TabsItem(label='G',tag="GPT"),
+    sac.TabsItem(label='G',tag="Ide"),
 
 ], align='wide', size='lg' , color='grape', use_container_width=True, return_index=True)
 
