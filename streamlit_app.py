22f8467765b0762136165ebadd2130e0cc5999c8
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-13 12:10:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,6 @@ if selecteds == 5:
             sys.stdout = new_stdout
             exec(code)
             sys.stdout = old_stdout
-            st.info("Output")
             output = new_stdout.getvalue()
             output_area_placeholder.text_area("Output:", value=output.strip(), height=300)
         except Exception as e:
