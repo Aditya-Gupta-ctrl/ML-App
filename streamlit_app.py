77314ae790fa6b845d4aa829bf5de87ce62f3500
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-19 11:05:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1401,12 +1401,30 @@ not in    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    #True if value is not found in
 """
         st.info(code14)
         st.write(" **Example of Menbership Operators in Python** ")
+        st.text("Input")
+        code15 = """
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if (x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
+"""
+        st.code(code15, language="python")
+        st.text("Output")
+        code16 = """
+x is NOT present in given list\n
+y is present in given list
+"""
+        st.success(code16)
         
-        
-
-
-
-
 
 
 
